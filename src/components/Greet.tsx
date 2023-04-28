153ad199fbbb30561c18ff57a9abd2b122c4bbf0
Ulas Ozturk <ozulashe@gmail.com>
2023-04-28 18:50:51 +0300

    Typing Props

---
+++ src/components/Greet.tsx
@@ -0,0 +1,19 @@
+type GreetProps = {
+  name: string;
+  messsageCount: number;
+  isLoggedIn: boolean;
+};
+
+export function Greet(props: GreetProps) {
+  const { messsageCount = 0 } = props;
+
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${messsageCount} unread messages`
+          : `Welcome Guest`}
+      </h2>
+    </div>
+  );
+}
