c95894796fab40840b339a3304170f2e9374dd8b
Ulas Ozturk <ozulashe@gmail.com>
2023-04-28 18:52:48 +0300

    Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 export function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Clark",
+      last: "Kent",
+    },
+    {
+      first: "Princess",
+      last: "Diana",
+    },
+  ];
+
   return (
     <>
       <Greet name="Ulas" messsageCount={5} isLoggedIn />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </>
   );
 }
